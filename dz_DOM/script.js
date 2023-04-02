7d5ccbb78693d3f53c277183f4d9b8a5d7d9cee3
KlimmedSoul <122864116+KlimmedSoul@users.noreply.github.com>
2023-04-03 00:30:15 +0300

    some changes

--- dz_DOM/script.js
+++ dz_DOM/script.js
@@ -1,16 +1,24 @@
 let programmingLenguages = [{
         value: "C++",
+        label: "C++"
     },
     {
         value: "Python",
+        label: "Python"
     },
     {
         value: "C#",
+        label: "C#"
     },
     {
         value: "Java",
+        label: "Java"
     }
 ]
+
+let randomArray = ["значение 1", 1, 4, "привет"]
+
+
 
 
 //                               ЗАДАНИЕ №1
@@ -22,32 +30,43 @@ let programmingLenguages = [{
 //выбранного по умолчанию элемента). По умолчанию выбирается 0-й элемент
 //массива значений. Если переданное значение не найдено в массиве элементов, то нужно также выбрать 0-й элемент.
 
-function getDropDownList(programmingLenguages, selectedValue = programmingLenguages[0].value) {
+function createDropDownList(arr, selectedValue = 0) {
 
-    let select = document.createElement("select");
-    document.body.append(select);
+    let select = document.createElement("select")
 
-    for (let i = 0; i < programmingLenguages.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
+        let elem = arr[i]
+        let label, value = null
 
-        let option = document.createElement("option");
-        select.append(option);
-        option.textContent = programmingLenguages[i].value;
-
-        if (option.value === selectedValue) {
-            option.setAttribute('selected', true);
+        if (typeof elem != "object") {
+            label = elem
+            value = elem
+        } else {
+            label = elem["label"]
+            value = elem["value"]
         }
 
+        let option = document.createElement("option")
+        option.setAttribute("value", value)
+        select.append(option)
 
+        option.textContent = label
+        elem == arr[0] ? option.setAttribute("value", selectedValue) : null
+
+        option.value === selectedValue ? option.setAttribute('selected', true) : null
     }
+
+    return select
 }
 
 
+document.body.append(createDropDownList(randomArray, "C#"))
+
+
+
 //                               ЗАДАНИЕ №2
-//Возьмите выполненное предыдущее задание. Сделайте так, чтобы функция создания
-//выпадающего списка могла обрабатывать не только массив объектов, но и другие виды входящих значений, а именно:
-//• Массив примитивных значений типа string или number. В таком случае value и label будут равны. Например: [1, 2, "три", "четыре", ...]
-//• Объект с произвольными свойствами и значениями типа string или number. В таком случае каждый элемент должен формироваться из ключа (value) и
-//значения (label) каждого свойства переданного объекта. Например: {valuel: 'Значение 1', value2: 'Значение 2', ...}
-
-
-// В процессе
+// Возьмите выполненное предыдущее задание. Сделайте так, чтобы функция создания
+// выпадающего списка могла обрабатывать не только массив объектов, но и другие виды входящих значений, а именно:
+// Массив примитивных значений типа string или number. В таком случае value и label будут равны. Например: [1, 2, "три", "четыре", ...]
+// Объект с произвольными свойствами и значениями типа string или number. В таком случае каждый элемент должен формироваться из ключа (value) и
+// значения (label) каждого свойства переданного объекта. Например: {valuel: 'Значение 1', value2: 'Значение 2', ...}
